a4bf55114c5daff659587e608dd8d7faaf5a830c
Thiago De Vargas <thiago.vargas001@gmail.com>
2022-06-02 17:44:20 -0300

    Adicionar arquivos de projeto.

---
+++ AdoCurso.API/Controllers/UsuariosController.cs
@@ -0,0 +1,69 @@
+﻿using AdoCurso.API.Models.Entities;
+using AdoCurso.API.Models.Interfaces;
+using AdoCurso.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdoCurso.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        #region Propriedades
+        private IUsuarioRepository _repository;
+        #endregion
+
+        #region Construtor
+        public UsuariosController()
+        {
+            _repository = new UsuarioRepository();
+        }
+        #endregion
+
+        #region Métodos
+        [HttpGet]
+        [Route("/Buscar")]
+        public IActionResult Get()
+        {
+            return Ok(_repository.GetAll());
+        }
+
+        [HttpGet]
+        [Route("/Buscar/{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _repository.GetById(id);
+            if (usuario == null)
+            {
+                return NotFound("Não existe usuário com este Id.");
+            }
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        [Route("/Cadastrar")]
+        public IActionResult Insert(Usuario usuario)
+        {
+            _repository.Insert(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut]
+        [Route("/Atualizar/{id}")]
+        public IActionResult Update(Usuario usuario)
+        {
+            _repository.Update(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete]
+        [Route("/Deletar/{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+        #endregion
+    }
+}
---
+++ AdoCurso.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,175 @@
+﻿using AdoCurso.API.Models.Entities;
+using AdoCurso.API.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoCurso.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly IDbConnection _connectionDB;
+        public UsuarioRepository()
+        {
+            _connectionDB = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AdoCurso;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
+        }
+
+        public List<Usuario> GetAll()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+
+            try
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM Usuarios",
+                                                    (SqlConnection)_connectionDB);
+
+                _connectionDB.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Usuario usuario = new Usuario();
+                    usuario.Id = reader.GetInt32("Id");
+                    usuario.Nome = reader.GetString("Nome");
+                    usuario.Email = reader.GetString("Email");
+                    usuario.Sexo = reader.GetString("Sexo");
+                    usuario.RG = reader.GetString("RG");
+                    usuario.CPF = reader.GetString("CPF");
+                    usuario.NomeMae = reader.GetString("NomeMae");
+                    usuario.SituacaoCadastro = reader.GetString("SituacaoCadastro");
+                    usuario.DataCadastro = reader.GetDateTimeOffset(8);
+
+                    usuarios.Add(usuario);
+                }
+            }
+            finally
+            {
+                _connectionDB.Close();
+            }
+
+            return usuarios;
+        }
+
+        public Usuario GetById(int id)
+        {
+            Usuario usuario = new Usuario();
+
+            try
+            {
+                SqlCommand command = new SqlCommand($"SELECT * FROM Usuarios WHERE Id = @Id",
+                                                    (SqlConnection)_connectionDB);
+                // Segurança contra SQL Injection
+                command.Parameters.AddWithValue("@Id", id);
+
+
+                _connectionDB.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    usuario.Id = reader.GetInt32("Id");
+                    usuario.Nome = reader.GetString("Nome");
+                    usuario.Email = reader.GetString("Email");
+                    usuario.Sexo = reader.GetString("Sexo");
+                    usuario.RG = reader.GetString("RG");
+                    usuario.CPF = reader.GetString("CPF");
+                    usuario.NomeMae = reader.GetString("NomeMae");
+                    usuario.SituacaoCadastro = reader.GetString("SituacaoCadastro");
+                    usuario.DataCadastro = reader.GetDateTimeOffset(8);
+
+                    return usuario;
+                }
+            }
+            finally
+            {
+                _connectionDB.Close();
+            }
+
+            return null;
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand($"INSERT INTO Usuarios(Nome, Email, Sexo, RG, CPF, NomeMae, SituacaoCadastro, DataCadastro) " +
+                                                    $"VALUES(@Nome, @Email, @Sexo, @RG, @CPF, @NomeMae, @SituacaoCadastro, @DataCadastro);" +
+                                                    $"SELECT CAST(scope_identity() AS int);", // Irá pegar o último id do escopo da tabela Usuarios.
+                                                    (SqlConnection)_connectionDB);
+
+                // Segurança contra SQL Injection
+                command.Parameters.AddWithValue("@Nome", usuario.Nome);
+                command.Parameters.AddWithValue("@Email", usuario.Email);
+                command.Parameters.AddWithValue("@Sexo", usuario.Sexo);
+                command.Parameters.AddWithValue("@RG", usuario.RG);
+                command.Parameters.AddWithValue("@CPF", usuario.CPF);
+                command.Parameters.AddWithValue("@NomeMae", usuario.NomeMae);
+                command.Parameters.AddWithValue("@SituacaoCadastro", usuario.SituacaoCadastro);
+                command.Parameters.AddWithValue("@DataCadastro", usuario.DataCadastro);
+
+
+                _connectionDB.Open();
+                usuario.Id = (int)command.ExecuteScalar(); // irá atribuir o último id da tabela ao id do usuario recem criado.
+            }
+            finally
+            {
+
+                _connectionDB.Close();
+
+            }
+        }
+
+        public void Update(Usuario usuario)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand($"UPDATE Usuarios SET Nome = @Nome, Email = @Email, Sexo = @Sexo, RG = @RG, CPF = @CPF, NomeMae = @NomeMae, SituacaoCadastro = @SituacaoCadastro, DataCadastro = @DataCadastro " +
+                                                    $"WHERE Usuarios.Id = @Id;",
+                                                    (SqlConnection)_connectionDB);
+
+                // Segurança contra SQL Injection
+                command.Parameters.AddWithValue("@Nome", usuario.Nome);
+                command.Parameters.AddWithValue("@Email", usuario.Email);
+                command.Parameters.AddWithValue("@Sexo", usuario.Sexo);
+                command.Parameters.AddWithValue("@RG", usuario.RG);
+                command.Parameters.AddWithValue("@CPF", usuario.CPF);
+                command.Parameters.AddWithValue("@NomeMae", usuario.NomeMae);
+                command.Parameters.AddWithValue("@SituacaoCadastro", usuario.SituacaoCadastro);
+                command.Parameters.AddWithValue("@DataCadastro", usuario.DataCadastro);
+
+                command.Parameters.AddWithValue("@Id", usuario.Id);
+
+                _connectionDB.Open();
+                command.ExecuteNonQuery();
+
+            }
+            finally
+            {
+
+                _connectionDB.Close();
+
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try 
+            {
+                SqlCommand command = new SqlCommand($"DELETE Usuarios WHERE Usuarios.Id = @Id;",
+                                                    (SqlConnection)_connectionDB);
+
+                command.Parameters.AddWithValue("@Id", id);
+
+                _connectionDB.Open();
+                command.ExecuteNonQuery();
+            } 
+            finally 
+            {
+                _connectionDB.Close();
+            }
+        }
+    }
+}

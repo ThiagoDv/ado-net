b643867cfce557f026ba6374e5d3ffe8afab7182
Thiago De Vargas <thiago.vargas001@gmail.com>
2022-06-03 15:44:28 -0300

    Instruções SQL

---
+++ ConsultasBD.sql
@@ -0,0 +1,5 @@
+﻿-- Instrução que pega os contatos do usuario comparando a chave estrangeira UsuarioId com o Id da tabela usuario.
+SELECT * FROM Usuarios u LEFT JOIN Contatos c ON c.UsuarioId = u.Id WHERE u.Id = 1;
+
+-- Instrução que pega os contatos e endereços do usuario comparando a chave estrangeira UsuarioId com o Id da tabela usuario.
+--SELECT * FROM Usuarios u LEFT JOIN Contatos c ON c.UsuarioId = u.Id LEFT JOIN EnderecosEntrega en ON en.UsuarioId = u.Id WHERE u.Id = 1;
